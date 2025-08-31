a44e998fa771f966259510fe3a6cae74ed5f07a8
CODE-BY-DHRUV <officialdhruvsharma18@gmail.com>
2025-08-31 22:11:47 +0530

    Keep only LibraryManagement.java in workshop branch

--- Day1_ProgrammingElements/CircleArea.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class CircleArea {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        double area = Math.PI * Math.pow(r,2);
-        System.out.println(area);
-    }
-}
--- Day1_ProgrammingElements/TemperatureConversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class TemperatureConversion {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int c = sc.nextInt();
-        int f = (c* 9/5) + 32;
-        System.out.println(f);
-    }
-}
